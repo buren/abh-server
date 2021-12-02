01ae9faab8c1a29298f4f4cd6c33926d47ca66ad
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-02 16:27:06 +0800

    Delete app directory

--- app/models/answer.rb
+++
@@ -1,7 +0,0 @@
-class Answer < ActiveRecord::Base
-  validates_presence_of :question, :answer
-
-  def self.answer_count(type)
-    where(question: type).group(:answer).count
-  end
-end
--- app/models/point.rb
+++
@@ -1,8 +0,0 @@
-class Point < ActiveRecord::Base
-  validates_presence_of :question, :x_value, :y_value
-
-  def self.question_result(type)
-    where(question: type)
-      .pluck(:x_value, :y_value)
-  end
-end
